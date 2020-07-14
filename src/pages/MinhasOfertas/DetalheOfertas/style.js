790214fed78e4e33b975acba9a41831b36576581
mmarcos29 <joaomarcos1916@gmail.com>
2020-07-14 17:48:52 -0300

    ajustando tela de detalheOfertas

--- src/pages/MinhasOfertas/DetalheOfertas/style.js
+++ src/pages/MinhasOfertas/DetalheOfertas/style.js
@@ -3,9 +3,9 @@ const windowWidth = Dimensions.get('window').width
 const windowHeight = Dimensions.get('window').height
 
 const Styles = StyleSheet.create({
-    container: { flex: 1, padding: "4%"},
-    box1: { height: "75%", backgroundColor: '#fff', elevation: 8, marginBottom:"10%" },
-    box2: { height: "20%", marginTop: "4%"},
+    container: { flex: 1, padding: "4%" },
+    box1: { height: "75%", backgroundColor: '#fff', elevation: 8, marginBottom: "10%" },
+    box2: { height: "20%", marginTop: "4%" },
 
     item1: { height: "20%", flexDirection: 'row' },
     item1_1: { width: "50%", backgroundColor: "#fff", marginLeft: 8 },
@@ -20,43 +20,81 @@ const Styles = StyleSheet.create({
         paddingTop: 10
     },
 
-    item2: { flexDirection: 'row', padding:"7%", paddingTop:"5%", borderTopWidth:0.3, paddingBottom:"2%"},
-    item2_1: { width: "60%", backgroundColor: "#fff"},    
-    item2_1Text: { fontFamily: "Montserrat-Medium", fontSize:15},    
+    item2: { flexDirection: 'row', padding: "7%", paddingTop: "5%", borderTopWidth: 0.3, paddingBottom: "2%" },
+    item2_1: { width: "60%", backgroundColor: "#fff" },
+    item2_1Text: { fontFamily: "Montserrat-Medium", fontSize: 15 },
     item2_2: { width: "45%", backgroundColor: "#fff" },
-    item2_2Text: { fontFamily: "Montserrat-Medium", fontSize:15},
+    item2_2Text: { fontFamily: "Montserrat-Medium", fontSize: 15 },
 
-    item3: { height: "8%", flexDirection: 'row', paddingTop:0, marginLeft:"5%", marginBottom:"4%"},
-    item3_1: { width: "50%", backgroundColor: "#fff", marginLeft:"2%"},    
-    item3_1Input: { textAlign:"center", borderWidth:3, borderColor: "#b4b4b4", borderRadius:25, width:"65%", fontSize:13, fontFamily:"Montserrat-light", borderColor: "#b4b4b4"},    
-    item3_2: { width: "35%", backgroundColor: "#fff", marginLeft:"5%" },
-    item3_2Input: { textAlign:"center", borderWidth:3, borderColor: "#b4b4b4", borderRadius:25, width:"95%", fontSize:13, fontFamily:"Montserrat-light", borderColor: "#b4b4b4"},
-    
-    item4: { flexDirection: 'row', padding:"7%", paddingTop:0, paddingBottom:"2%"},
-    item4_1: { width: "60%", backgroundColor: "#fff"},    
-    item4_1Text: { fontFamily: "Montserrat-Medium", fontSize:15},    
-    
-    item5: { height: "8%", flexDirection: 'row', paddingTop:0, marginLeft:"5%", marginBottom:"4%"},
-    item5_1: { width: "100%", backgroundColor: "#fff", paddingLeft:"2%"},    
-    item5_1Input: { textAlign:"center", borderWidth:3, borderColor: "#b4b4b4", borderRadius:25, width:"90%", fontSize:13, fontFamily:"Montserrat-light", borderColor: "#b4b4b4"},    
-    
-    item6: { flexDirection: 'row', padding:"7%", paddingTop:0, paddingBottom:"2%"},
-    item6_1: { width: "60%", backgroundColor: "#fff"},    
-    item6_1Text: { fontFamily: "Montserrat-Medium", fontSize:15},    
-    
-    item7: { height: "8%", flexDirection: 'row', paddingTop:0, marginLeft:"5%", marginBottom:"4%"},
-    item7_1: { width: "100%", backgroundColor: "#fff", paddingLeft:"2%"},    
-    item7_1Input: { textAlign:"center", borderWidth:3, borderColor: "#b4b4b4", borderRadius:25, width:"90%", fontSize:13, fontFamily:"Montserrat-light", borderColor: "#b4b4b4"},    
+    item3: { height: "8%", flexDirection: 'row', paddingTop: 0, marginLeft: "5%", marginBottom: "4%" },
+    item3_1: { width: "50%", backgroundColor: "#fff", marginLeft: "2%" },
+    item3_1Input: {
+        textAlign: "center",
+        backgroundColor: "#fff",
+        borderColor: "#d0d0d0",
+        elevation: 6,
+        borderRadius: 25,
+        width: "65%",
+        fontSize: 13,
+        fontFamily: "Montserrat-light",
+        borderColor: "#b4b4b4"
+    },
+    item3_2: { width: "35%", backgroundColor: "#fff", marginLeft: "5%" },
+    item3_2Input: {
+        elevation: 6,
+        backgroundColor: "#fff",
+        textAlign: "center",
+        borderColor: "#d0d0d0",
+        borderRadius: 25,
+        width: "95%",
+        fontSize: 13,
+        fontFamily: "Montserrat-light",
+    },
+    item4: { flexDirection: 'row', padding: "7%", paddingTop: 0, paddingBottom: "2%" },
+    item4_1: { width: "60%", backgroundColor: "#fff" },
+    item4_1Text: { fontFamily: "Montserrat-Medium", fontSize: 15 },
+
+    item5: { height: "8%", flexDirection: 'row', paddingTop: 0, marginLeft: "5%", marginBottom: "4%" },
+    item5_1: { width: "100%", backgroundColor: "#fff", paddingLeft: "2%" },
+    item5_1Input: {
+        elevation: 6,
+        backgroundColor:"#fff", 
+        textAlign:"center",
+        borderColor: "#d0d0d0", 
+        textAlign: "center",         
+        borderRadius: 25, 
+        width: "90%", 
+        fontSize: 13, 
+        fontFamily: "Montserrat-light",
+    },
+
+    item6: { flexDirection: 'row', padding: "7%", paddingTop: 0, paddingBottom: "2%" },
+    item6_1: { width: "60%", backgroundColor: "#fff" },
+    item6_1Text: { fontFamily: "Montserrat-Medium", fontSize: 15 },
+
+    item7: { height: "8%", flexDirection: 'row', paddingTop: 0, marginLeft: "5%", marginBottom: "4%" },
+    item7_1: { width: "100%", backgroundColor: "#fff", paddingLeft: "2%" },
+    item7_1Input: {
+        elevation: 6,
+        backgroundColor:"#fff", 
+        textAlign:"center",
+        borderColor: "#d0d0d0", 
+        textAlign: "center",         
+        borderRadius: 25, 
+        width: "90%", 
+        fontSize: 13, 
+        fontFamily: "Montserrat-light",
+    },
 
 
-    item8: { height: "8%", flexDirection: 'row', marginTop:"10%" },
-    item8_1: { width: "100%", backgroundColor: "#fff", alignItems:"center"},    
-    item8_1Text: { borderRadius:25, width:"60%", backgroundColor:"#9C3F3A", textAlign:"center", padding:"2%", fontSize:16, fontFamily:"Montserrat-SemiBold", color:"white"},    
-    
-    
-    item9: { height: "60%", elevation: 4, backgroundColor: "#fff", padding:7 },
-    item9_1: { width: "100%", height:"100%", backgroundColor:"#ff0000", alignItems:"center", justifyContent:"center", elevation:2},
-    item9_1Text: { fontFamily: 'Montserrat-Medium', fontSize: 22, textAlign:"center", color:"white" }
+    item8: { height: "8%", flexDirection: 'row', marginTop: "7%" },
+    item8_1: { width: "100%", backgroundColor: "#fff", alignItems: "center" },
+    item8_1Text: { borderRadius: 25, width: "60%", backgroundColor: "#9C3F3A", textAlign: "center", padding: "2%", fontSize: 16, fontFamily: "Montserrat-SemiBold", color: "white" },
+
+
+    item9: { height: "60%", elevation: 4, backgroundColor: "#fff", padding: 7 },
+    item9_1: { width: "100%", height: "100%", backgroundColor: "#ff0000", alignItems: "center", justifyContent: "center", elevation: 2 },
+    item9_1Text: { fontFamily: 'Montserrat-Medium', fontSize: 22, textAlign: "center", color: "white" }
 
 
 })
