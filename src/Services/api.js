88b30e5fede8c9b9fe7f79262d1637c88623252a
williame <williame_lima@hotmail.com>
2020-07-16 21:56:32 -0300

    produtos alteraçõe de produtos

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://52.14.100.212:11000/api/',
-    baseURL: 'https://a214bbd19df2.ngrok.io/api/'
+    baseURL: 'http://52.14.100.212:11000/api/',
+    //baseURL: 'https://a214bbd19df2.ngrok.io/api/'
 });
 
 export default api;
