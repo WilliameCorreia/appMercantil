202ee10e6d378b4b7d541c7dd943957c578f5b5b
williame <williame_lima@hotmail.com>
2020-07-17 21:35:33 -0300

    refatornado AuthContext

--- src/Contexts/Auth.js
+++ src/Contexts/Auth.js
@@ -1,7 +1,8 @@
 import React, { createContext, useState, useEffect } from 'react'
-import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native'
 
-//import * as auth from '../Services/Auth'
+import auth from '@react-native-firebase/auth'
+import Api from '../Services/api'
 
 const AuthContext = createContext({ signed: false })
 
@@ -9,32 +10,49 @@ export const AuthProvider = ({ children }) => {
 
     const [usuario, SetUsuario] = useState({ email: '', token: '' })
     const [loading, SetLoading] = useState(true)
-    //const [token, SetToken] = useState(null)
-
+    const [estabelecimento, setEstabelecimento] = useState(false);
     async function signIn(user) {
         setTimeout(() =>{
             if (user) {
-                const { email, uid } = user
-                SetUsuario({ email, uid })
+                GetEstabelecimento(user);
             }else{
                 SetLoading(false)
                 SetUsuario({email: undefined})
-                console.log("entro aqui")
             }
         },2000)
+    }
+
+    async function GetEstabelecimento(user) {
+        const { email, uid } = user;
+        console.log(uid)
+        Api.get(`Estabelecimento/${uid}`).then(response => {
+            let estabelecimento = response.data;
+            if ( estabelecimento != null ) {
+                setEstabelecimento(estabelecimento) 
+            }else{
+                setEstabelecimento(null);
+            }
+            SetUsuario({ email, uid })
+        }).catch(
+            erro => {
+                Alert.alert("não estabelecer conexão com a base de dados");
+                SetLoading(false)
+            }
+        );
     }
 
     useEffect(() => {
         const subscriber = auth().onUserChanged(signIn);
         return () => {
-            subscriber; // unsubscribe on unmount
+            console(subscriber);
+             // unsubscribe on unmount
         }
     }, []);
 
     console.log("authContext => " + usuario.email)
 
     return (
-        <AuthContext.Provider value={{ signed: Boolean(usuario.email), signIn, usuario, loading }}>
+        <AuthContext.Provider value={{ signed: Boolean(usuario.email), signIn, usuario, loading, estabelecimento }}>
             {children}
         </AuthContext.Provider>
     )
